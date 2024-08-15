d62df2cc7d6fd7e5aca62f2866beac8f7ca8e692
rahma aulia <qwertyrahmaaulia@gmail.com>
2024-08-15 11:26:50 +0700

    feat: put status study plan

--- app/src/app/api/user/studyplan/route.ts
+++ app/src/app/api/user/studyplan/route.ts
@@ -1,42 +1,61 @@
-import Studyplan from "@/db/models/Studyplan";
+import Studyplan, { Plan } from "@/db/models/Studyplan";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    try {
-        const { searchParams } = new URL(request.url);
-        const chapterId = searchParams.get("chapterId") as string;
-        const userId = request.headers.get("x-id") as string;
-        let data = await Studyplan.getPlanByChapterId(chapterId, userId);
-        if (!data) {
-            return NextResponse.json(
-                { message: "You don't have a study plan yet." },
-                { status: 404 }
-            );
-        }
-        return NextResponse.json(data, { status: 200 });
-    } catch (error: any) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
+  try {
+    const { searchParams } = new URL(request.url);
+    const chapterId = searchParams.get("chapterId") as string;
+    const userId = request.headers.get("x-id") as string;
+    let data = await Studyplan.getPlanByChapterId(chapterId, userId);
+    if (!data) {
+      return NextResponse.json(
+        { message: "You don't have a study plan yet." },
+        { status: 404 }
+      );
     }
+    return NextResponse.json(data, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
 
-export async function DELETE(request:Request){
-    try {
-        const { searchParams } = new URL(request.url);
-        const chapterId = searchParams.get("chapterId") as string;
-        const userId = request.headers.get("x-id") as string;
-        console.log(chapterId, userId);
-        
-        let data = await Studyplan.deletePlanByChapterId(chapterId, userId);
-        
-        if (data.deletedCount === 0) {
-            return NextResponse.json(
-                { message: "You don't have a study plan yet." },
-                { status: 404 }
-            );
-        }
-        return NextResponse.json({ message: "Study plan deleted." }, { status: 200 });
-    } catch (error: any) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
+export async function DELETE(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const chapterId = searchParams.get("chapterId") as string;
+    const userId = request.headers.get("x-id") as string;
+    console.log(chapterId, userId);
+    let data = await Studyplan.getPlanByChapterId(chapterId, userId);
+    await Studyplan.deletePlanByChapterId(chapterId, userId);
+    if (!data) {
+      return NextResponse.json(
+        { message: "You don't have a study plan yet." },
+        { status: 404 }
+      );
     }
-    
+    return NextResponse.json(
+      { message: "Study plan deleted." },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    const body: {
+      _id: string;
+      chapterId: string;
+      plan_contents: Array<Plan>;
+      userId: string;
+    } = await request.json();
+    const userId = request.headers.get("x-id") as string;
+    let data = await Studyplan.updatePlan(body._id, body);
+    console.log(body, userId);
+    return NextResponse.json(data, { status: 200 });
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
--- app/src/db/models/Studyplan.ts
+++ app/src/db/models/Studyplan.ts
@@ -2,32 +2,47 @@ import { ObjectId } from "mongodb";
 import { database } from "../config/mongodb";
 
 export interface planType {
-    chapterId: string;
-    plan: Array<Plan>;
+    _id?: string;
+  chapterId: string;
+  plan_contents: Array<Plan>;
+  userId: string;
 }
 
 export interface Plan {
-    judulTask: string;
-    taskList: Array<string>;
-    status: false;
+  judulTask: string;
+  taskList: Array<string>;
+  status: false;
 }
 
 class Studyplan {
-    static collection() {
-        return database.collection("studyplans");
-    }
-    static async createPlan(userId: string, subjectId: string, planOutput: planType) {
-        return await this.collection().insertOne({
-            chapterId: new ObjectId(String(subjectId)),
-            plan_contents: planOutput.plan,
-            userId: new ObjectId(String(userId)),
-        });
-    }
-    static async getPlanByChapterId(chapterId: string, userId: string) {
-        return await this.collection().findOne({ chapterId: new ObjectId(chapterId), userId : new ObjectId(userId) });
-    }
-    static async deletePlanByChapterId(chapterId: string, userId: string) {
-        return await this.collection().deleteOne({ chapterId: new ObjectId(chapterId), userId : new ObjectId(userId) });
-    }
+  static collection() {
+    return database.collection("studyplans");
+  }
+  static async createPlan(
+    userId: string,
+    subjectId: string,
+    planOutput: planType
+  ) {
+    return await this.collection().insertOne({
+      chapterId: new ObjectId(String(subjectId)),
+      plan_contents: planOutput.plan_contents,
+      userId: new ObjectId(String(userId)),
+    });
+  }
+  static async getPlanByChapterId(chapterId: string, userId: string) {
+    return await this.collection().findOne({
+      chapterId: new ObjectId(chapterId),
+      userId: new ObjectId(userId),
+    });
+  }
+  static async deletePlanByChapterId(chapterId: string, userId: string) {
+    return await this.collection().deleteOne({
+      chapterId: new ObjectId(String(chapterId)),
+      userId: new ObjectId(String(userId)),
+    });
+  }
+  static async updatePlan(_id: string, data: planType){
+    return await this.collection().updateOne({_id: new ObjectId(String(_id))}, {$set: {plan_contents: data.plan_contents}})
+  }
 }
 export default Studyplan;
